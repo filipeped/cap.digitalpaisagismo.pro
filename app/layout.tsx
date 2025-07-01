25da59b1453c6d5653e7c599f4c8056ca0ed1f82
filipeped <filipepedoo@gmail.com>
2025-06-30 21:05:36 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
+export const metadata = {
+  title: "Proxy CAPI",
+  description: "Diagnóstico do Proxy CAPI"
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  );
+  )
 }
